83a0c9ef9418e9e7f514f5e3a2e9159f9a8d4916
guaiu_glb <guiau_glb@gmail.com>
2020-11-21 08:49:01 +0800

    20201121[demo]feat feat: 添加了pyinstaller文件夹，学习使用pyinstaller将.py文件打包成.exe文件

---
+++ demo/pyinstaller/README.md
@@ -0,0 +1,2 @@
+demo1 使用pyinstaller打包.py文件成为.exe文件
+命令行 pyinstaller.exe -F demo1.py -w
---
+++ demo/pyinstaller/demo1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# demo1 使用pyinstaller打包.py文件成为.exe文件
+# 命令行 pyinstaller.exe -F demo1.py -w
+
+import sys
+from PySide2.QtWidgets import QApplication,QMainWindow,QFileDialog
+import mainUi
+class MainCode(QMainWindow,mainUi.Ui_MainWindow):
+	def __init__(self):
+		QMainWindow.__init__(self)
+		mainUi.Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.btn_save.clicked.connect(self.on_save)
+		self.btn_open.clicked.connect(self.on_open)
+ 
+	def on_save(self):
+		FullFileName,_=QFileDialog.getSaveFileName (self, '文件另存为', r'./','TXT (*.txt)')
+		set_text=self.txt_view.toPlainText()
+		with open(FullFileName,'wt') as f:
+			print(set_text, file = f)
+ 
+	def on_open(self):
+		txtstr=""
+		FullFileName, _ = QFileDialog.getOpenFileName(self, '打开', r'./', 'TXT (*.txt)')
+		with open(FullFileName, 'rt') as f:
+			lines=f.readlines()
+			for line in lines:
+				txtstr=txtstr+line
+		self.txt_view.setText(txtstr)
+ 
+ 
+if __name__=='__main__':
+	app=QApplication(sys.argv)
+	md=MainCode()
+	md.show()
+	sys.exit(app.exec_())
